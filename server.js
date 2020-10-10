9a97a2e68d7f6b065f2d7261b50a938fdebe8fe2
Richard Allen <47337325+Rallen52@users.noreply.github.com>
2020-10-09 21:43:26 -0400

    using cors in server.js

--- server.js
+++ server.js
@@ -15,5 +15,5 @@ app.get("/budget", (req, res) => {
 });
 
 app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`);
+  console.log(`API listening at http://localhost:${port}`);
 });
